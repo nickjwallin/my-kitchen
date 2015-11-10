c9a3de8cfbeaf40b35305a2c46280c6acadd9dda
Nicholas J. Wallin <nickjwallin@gmail.com>
2015-11-10 13:43:05 -0600

    Adding Add Ingredient View Controller, hooking up Cancel button to dismiss

---
+++ MyKitchen/AddIngredientViewController.h
@@ -0,0 +1,15 @@
+//
+//  AddIngredientViewController.h
+//  MyKitchen
+//
+//  Created by Nicholas Wallin on 11/10/15.
+//  Copyright © 2015 Nicholas J. Wallin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddIngredientViewController : UIViewController
+
+- (IBAction)cancelButtonTapped:(id)sender;
+
+@end

6d7d130d9aef5e70b1a5e6e444328d827f7b7676
Nicholas J. Wallin <nickjwallin@gmail.com>
2015-11-10 21:57:53 -0600

    Adding Add Recipe View Controller, hooking up Cancel button to dismiss

---
+++ MyKitchen/AddRecipeViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddRecipeViewController.h
+//  MyKitchen
+//
+//  Created by Nicholas Wallin on 11/10/15.
+//  Copyright © 2015 Nicholas J. Wallin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AddRecipeViewController : UIViewController
+
+- (IBAction)cancelButtonTapped:(id)sender;
+- (IBAction)addButtonTapped:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+
+@end
---
+++ MyKitchen/AddRecipeViewController.m
@@ -0,0 +1,55 @@
+//
+//  AddRecipeViewController.m
+//  MyKitchen
+//
+//  Created by Nicholas Wallin on 11/10/15.
+//  Copyright © 2015 Nicholas J. Wallin. All rights reserved.
+//
+
+#import "AddRecipeViewController.h"
+
+@interface AddRecipeViewController ()
+
+@end
+
+@implementation AddRecipeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)cancelButtonTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)addButtonTapped:(id)sender {
+}
+
+#pragma mark - Core Data
+
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+@end
